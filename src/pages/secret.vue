f8f4081567f7c3a19fd7eb3a69089e7e0b3a3173
jvill171 <juanvillarreal64@yahoo.com>
2021-11-10 11:44:19 -0600

    Modified Secret Page

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,3 +1,8 @@
-<template>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veritatis expedita laboriosam repellat ducimus facilis dicta aliquam deleniti nemo ad! Asperiores ipsa perferendis sapiente porro nihil illum architecto totam neque.</p>
+
+<template >
+<div class="flex-col flex items-center justify-center min-h-screen-nonav">
+    <h2 class=" text-6xl font-extralight tracking-tighter z-50 pb-4">Secret Page</h2>
+    <img src="../assets/bg_secret.png" class="border-2 border-pink-400 rounded-t-lg" alt="Secret BG">
+    <p class="border-2 border-t-0 border-pink-400 bg-pink-300 rounded-b-lg px-4 text-lg text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veritatis expedita laboriosam repellat ducimus facilis dicta aliquam deleniti nemo ad! Asperiores ipsa perferendis sapiente porro nihil illum architecto totam neque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur est impedit illo saepe quisquam delectus numquam cupiditate vel non animi, similique tempore, nisi labore et pariatur, illum unde tempora inventore?</p>
+</div>
 </template>
